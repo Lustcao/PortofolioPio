77db6f6623d66184eb6d58ba394df77c75ef26a7
Tech <najla.noviolita37@smk.belajar.id>
2025-10-01 10:27:38 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        hijau1: '#101916',
+        hijau2: '#101511',
+        hijau3: '#222922',
+        hijau4: '#3d4633',
+        hijau5: '#717476',
+      }
+    },
+  },
+  plugins: [],
+}
+
